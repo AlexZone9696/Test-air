32926a3b55e62c7f410a4ab8568eb6f8e065f90d
AlexZone9696 <159000422+AlexZone9696@users.noreply.github.com>
2025-06-18 09:47:50 +0500

    Create send.js

---
+++ api/send.js
@@ -0,0 +1,37 @@
+const TOKEN = "7956166311:AAH-RgLDYMaC3bjWoyQkfe3dQBKr3mKdSq0"; // Замените на токен вашего бота
+const CHAT_ID = "@adschannelapp_bot"; // Или -100xxxxxxxxxx
+
+export default async function handler(req, res) {
+  const message = `Go to our <b>backup channel</b>\n\nLegal earning schemes only here 👇🏻`;
+
+  const replyMarkup = {
+    inline_keyboard: [
+      [
+        {
+          text: "GO 🚀",
+          url: "https://t.me/AdBeastClickBot"
+        }
+      ]
+    ]
+  };
+
+  const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: message,
+      parse_mode: "HTML",
+      reply_markup: replyMarkup
+    })
+  });
+
+  if (response.ok) {
+    res.status(200).json({ status: "ok", message: "Message sent" });
+  } else {
+    const error = await response.text();
+    res.status(500).json({ status: "error", error });
+  }
+}
